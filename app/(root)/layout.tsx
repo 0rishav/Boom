e065d0f4c7732136788b48fb5a08a27ed3dc92d6
0rishav <rishavrajraj1155@gmail.com>
2024-04-12 02:45:02 +0530

    first-commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Boom",
+  description: "Video Calling App",
+  icons:{
+    icon:"/icons/logo.jpg"
+  }
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
